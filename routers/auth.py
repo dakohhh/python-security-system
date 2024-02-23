7e0eb02a830035537894e01e34b86d10f1b00242
dakohhh <wiizzydreadmill@gmail.com>
2024-02-23 02:06:57 -0800

    chore: updated login route

--- routers/auth.py
+++ routers/auth.py
@@ -10,10 +10,11 @@ from exceptions.custom_exception import BadRequestException
 router = APIRouter(tags=["Auth"], prefix="/auth")
 
 
-
 @router.post("/login")
 async def login_user(request: Request, login_input: LoginSchema):
 
     token = await auth.authenticate_user(login_input)
 
-    return CustomResponse("login user successfully", data=token)
+    context = {"token": token}
+
+    return CustomResponse("login user successfully", data=context)
